415a2554905e77813d5ffda856b02b130990d702
Nando <srluisfernando0@gmail.com>
2024-09-26 05:28:18 -0300

    fix Vector2D.dot method

--- src/core/LinearAlgebra.ts
+++ src/core/LinearAlgebra.ts
@@ -1,10 +1,27 @@
 export class Vector2D {
-	public readonly x: number;
-	public readonly y: number;
+	public x: number;
+	public y: number;
 
 	public constructor(x: number, y: number) {
 		this.x = x;
 		this.y = y;
+	}
+
+
+
+	/*
+		Methods
+	*/
+	public clone(): Vector2D {
+		return new Vector2D(this.x, this.y);
+	}
+
+	public length(): number {
+		return Math.sqrt(this.sqrLength());
+	}
+
+	public sqrLength(): number {
+		return Vector2D.dot(this, this);
 	}
 
 
@@ -15,8 +32,6 @@ export class Vector2D {
 	public static zero(): Vector2D {
 		return new Vector2D(0, 0);
 	}
-
-
 
 	public static add(u: Vector2D, v: Vector2D): Vector2D {
 		return new Vector2D(u.x + v.x, u.y + v.y);
@@ -34,7 +49,19 @@ export class Vector2D {
 		return Vector2D.mul(1/k, u);
 	}
 
-	public static dot(u: Vector2D, v: Vector2D): Vector2D {
-		return new Vector2D(u.x*v.x, u.y*v.y);
+	public static dot(u: Vector2D, v: Vector2D): number {
+		return u.x*v.x + u.y*v.y;
 	}
+
+	/*public static min(u: Vector2D, v: Vector2D): Vector2D {
+		let minX = (u.x < v.x) ? u.x : v.x;
+		let minY = (u.y < v.y) ? u.y : v.y;
+		return new Vector2D(minX, minY);
+	}
+
+	public static max(u: Vector2D, v: Vector2D): Vector2D {
+		let maxX = (u.x > v.x) ? u.x : v.x;
+		let maxY = (u.y > v.y) ? u.y : v.y;
+		return new Vector2D(maxX, maxY);
+	}*/
 }
